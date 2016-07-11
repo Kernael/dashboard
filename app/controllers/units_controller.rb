62bd56700ec7bb948498d110e14d52be8a2f41b9
Eliot BARDET <eliot.bardet@gmail.com>
2016-07-11 16:20:15 +0200

    Unit : index rows link to show

--- app/controllers/units_controller.rb
+++ app/controllers/units_controller.rb
@@ -1,6 +1,16 @@
 class UnitsController < ApplicationController
+  before_action :set_unit, only: %i(show)
 
   def index
     @units = Unit.all
+  end
+
+  def show
+  end
+
+  private
+
+  def set_unit
+    @unit = Unit.find(params[:id])
   end
 end
