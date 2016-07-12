fbcabceb2fe49061c5be2087743982469b42e407
Eliot BARDET <eliot.bardet@gmail.com>
2016-07-12 12:15:40 +0200

    Unit : basic auth for protected units

--- app/controllers/units_controller.rb
+++ app/controllers/units_controller.rb
@@ -1,5 +1,7 @@
 class UnitsController < ApplicationController
   before_action :set_unit, only: %i(show)
+  http_basic_authenticate_with name: Rails.application.secrets.user,
+    password: Rails.application.secrets.password, only: :show, if: :need_auth?
 
   def index
     @units = Unit.all
@@ -12,5 +14,9 @@ class UnitsController < ApplicationController
 
   def set_unit
     @unit = Unit.find(params[:id])
+  end
+
+  def need_auth?
+    @unit.protected?
   end
 end
